ff4a0d6218a24b713a2246cf09af7d3caeb70c67
Thiago Cavalcante <thiagopcsantos@gmail.com>
2019-04-20 21:49:17 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+    this.state = { 
+      tasks: [
+        "Yo creo en milagro", 
+        "Lista de compras", 
+        "React seu maldito", 
+        "fazer o todo e enviar",
+        "Quem inventou o react?"
+        ],
+        inputElement: ''
+    };
+    
+    //this.handleChange = this.handleChange.bind(this)
+  }
+  
+  enviar(evt){
+    evt.preventDefault();
+    console.log(this.state.inputElement)
+    this.setState({
+      tasks: this.state.tasks.concat([this.state.inputElement]),
+      inputElement: ''
+    })
+  } 
+  
+  handleChange(evt) {
+    this.setState({ inputElement: evt.target.value});
+  }
+  
+
   render() {
+    const respuesta = this.state.tasks.map((lista, i) => {
+      return <li key={i} className="list-group-item"> {lista} </li>;
+    });
+  
+    let titulo = <h1>TODOS</h1>;  
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {titulo}
+        <div className="input-group mb-3">
+          <form onSubmit={this.enviar.bind(this)}>
+           <input type="text" className="form-control" value={this.state.inputElement} onChange={this.handleChange.bind(this)}/>
+          </form>
+        </div>
+        <ul className="list-group">
+          {respuesta} 
+        </ul>
       </div>
     );
   }
